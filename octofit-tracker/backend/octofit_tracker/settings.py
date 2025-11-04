6f259e6f6dd8a5a8cc5193060a6c43fa49e962da
YingShawn <ying.xiong@accenture.com>
2025-11-04 14:44:10 +0000

    chore: support codespace and localhost API URLs in Django settings and api_root

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-_nh0dsmgfmaa*dc+gbb0_=r#4twn+*9^_anwi0#ovb#^ucy*i9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
